0021b58c281c3f5bd59c18eca020449e910791be
Eric Dong <eric2043@gmail.com>
2024-09-15 23:12:11 -0400

    Replaced vimscript with lua api

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,7 +1,10 @@
 -- Setup our JDTLS server any time we open up a java file
-vim.cmd([[
-    augroup jdtls_lsp
-        autocmd!
-        autocmd FileType java lua require("config.jdtls").setup_jdtls()
-    augroup end
-]])
+local group_id = vim.api.nvim_create_augroup("jdtls_lsp", {})
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "java" },
+	group = group_id,
+	callback = function()
+		require("config.jdtls.setup").setup_jdtls()
+	end,
+})
+
