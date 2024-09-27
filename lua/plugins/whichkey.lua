d8503c017fa2171fb4ffba796cd72000836b4a92
Eric Dong <eric@elissas-mbp.myfiosgateway.com>
2024-09-26 23:43:09 -0400

    Ran formatter on all configs

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,22 +1,22 @@
 return {
-    "folke/which-key.nvim",
-    event = "VimEnter",
-    config = function()
-        -- gain access to the which key plugin
-        local which_key = require("which-key")
+	"folke/which-key.nvim",
+	event = "VimEnter",
+	config = function()
+		-- gain access to the which key plugin
+		local which_key = require("which-key")
 
-        -- Register prefixes for the different key mappings we have setup previously
-        which_key.setup({
-            spec = {
-                { "<leader>/", group = "Comments" },
-                { "<leader>c", group = "[C]ode" },
-                { "<leader>d", group = "[D]ebug" },
-                { "<leader>e", group = "[E]xplorer" },
-                { "<leader>f", group = "[F]ind" },
-                { "<leader>g", group = "[G]it" },
-                { "<leader>J", group = "[J]ava" },
-                { "<leader>w", group = "[W]indow" },
-            },
-        })
-    end,
+		-- Register prefixes for the different key mappings we have setup previously
+		which_key.setup({
+			spec = {
+				{ "<leader>/", group = "Comments" },
+				{ "<leader>c", group = "[C]ode" },
+				{ "<leader>d", group = "[D]ebug" },
+				{ "<leader>e", group = "[E]xplorer" },
+				{ "<leader>f", group = "[F]ind" },
+				{ "<leader>g", group = "[G]it" },
+				{ "<leader>J", group = "[J]ava" },
+				{ "<leader>w", group = "[W]indow" },
+			},
+		})
+	end,
 }
